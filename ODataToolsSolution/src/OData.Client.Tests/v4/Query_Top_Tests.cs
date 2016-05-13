f1f7f538412c8a9b7d3f5e0ff43e7131f440e3e3
macksm63 <macksm63@yahoo.com>
2016-05-12 22:43:23 -0600

    Added $top and $skip and a few tests

---
+++ ODataToolsSolution/src/OData.Client.Tests/v4/Query_Top_Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scrumfish.OData.Client.Tests.TestObjects;
+using Scrumfish.OData.Client.v4;
+
+namespace Scrumfish.OData.Client.Tests.v4
+{
+    [TestClass]
+    public class Query_Top_Tests
+    {
+        [TestMethod]
+        public void Top_AddsExpectedParameterName_Test()
+        {
+            var expected = "?$top=";
+            var result = "?".Top(5);
+            Assert.IsTrue(result.StartsWith(expected));
+        }
+
+        [TestMethod]
+        public void Top_AddsExpectedParameterNameWithAmpersand_Test()
+        {
+            var expected = "hello&$top";
+            var result = "hello".Top(5);
+            Assert.IsTrue(result.StartsWith(expected));
+        }
+    }
+}
