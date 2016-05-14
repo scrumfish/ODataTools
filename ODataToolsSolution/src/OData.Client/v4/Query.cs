1b04b7bbf3609c68c68fb2ae5f7b9c0a50fd2498
SteveReece <me@stephenreece.com>
2016-05-13 21:31:10 -0600

    Finished the merge.  Reformatted the base code.

--- ODataToolsSolution/src/OData.Client/v4/Query.cs
+++ ODataToolsSolution/src/OData.Client/v4/Query.cs
@@ -9,27 +9,25 @@ namespace Scrumfish.OData.Client.v4
 
         public static string Filter<T>(this string target, Expression<Func<T, object>> action) where T : class
         {
-            var filter = target.GetStartQuery()
+           return target.GetStartQuery()
                 .Append("$filter=")
                 .Append(action.GetLambdaBody()
-                    .ParseExpression());
-            return filter.ToString();
+                    .ParseExpression())
+                .ToString();
         }
 
         public static string Top(this string target, int topNum)
         {
-            var top = target.GetStartQuery()
-               .AppendFormat("$top={0}", topNum);
-
-            return top.ToString();
+            return target.GetStartQuery()
+                .AppendFormat("$top={0}", topNum)
+                .ToString();
         }
 
         public static string Skip(this string target, int skipNum)
         {
-            var skip = target.GetStartQuery()
-               .AppendFormat("$skip={0}", skipNum);
-
-            return skip.ToString();
+            return target.GetStartQuery()
+               .AppendFormat("$skip={0}", skipNum)
+               .ToString();
         }
     }
 }
