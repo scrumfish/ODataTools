80d2393174c97795729325b305bc528f547d65ff
SteveReece <me@stephenreece.com>
2016-07-06 19:58:24 -0600

    Added System.Spatial.GeographyPoint support.

--- ODataToolsSolution/src/OData.Client/Factories/StringifierFactory.cs
+++ ODataToolsSolution/src/OData.Client/Factories/StringifierFactory.cs
@@ -7,23 +7,34 @@ namespace Scrumfish.OData.Client.Factories
     internal static class StringifierFactory
     {
 
-        private static Dictionary<string, string> _providedLibraries = null;
+        private static readonly Dictionary<Type, Type> ProvidedLibraries = GetProviderLibaries();
+
+        private static Dictionary<Type, Type> GetProviderLibaries()
+        {
+            var result = new Dictionary<Type, Type>();
+            var providers = TypeFactoryConfiguration.GetConfiguration();
+            for (var i = 0; i < providers.Stringifiers.Count; i++)
+            {
+                var dataType = Type.GetType(providers.Stringifiers[i].TypeName);
+                var stringifierType = Type.GetType(providers.Stringifiers[i].StringifierName);
+                result.Add(dataType, stringifierType);
+            }
+            return result;
+        }
 
         public static Stringifier GetStringifier(object target)
         {
             var library = FindLibrary(target.GetType());
             if (library == null) return null;
-            var type = Type.GetType(library);
-            if (type == null) return null;
-            var result = Activator.CreateInstance(type, target) as Stringifier;
+            var result = Activator.CreateInstance(library, target) as Stringifier;
             return result;
         }
 
-        private static string FindLibrary(Type type)
+        private static Type FindLibrary(Type type)
         {
             if (type == null) return null;
-            string library;
-            _providedLibraries.TryGetValue(type.AssemblyQualifiedName, out library);
+            Type library;
+            ProvidedLibraries.TryGetValue(Type.GetType(type.AssemblyQualifiedName), out library);
             return library ?? FindLibrary(type.BaseType);
         }
     }
